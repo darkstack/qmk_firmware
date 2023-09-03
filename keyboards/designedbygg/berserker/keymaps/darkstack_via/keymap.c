63abad8b22c0109faee2a24b0dc2dcb4bdde6132
darkstack <1835601+darkstack@users.noreply.github.com>
2023-09-03 14:21:16 +0200

    Modification config layout

--- keyboards/designedbygg/berserker/keymaps/darkstack_via/keymap.c
+++ keyboards/designedbygg/berserker/keymaps/darkstack_via/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdalign.h>
 #include <sys/types.h>
 #include QMK_KEYBOARD_H
 
@@ -97,6 +98,8 @@ typedef struct _indicator_config_t {
 typedef struct _berserker_config_t {
     indicator_config caps_lock;
     indicator_config num_lock;
+    indicator_config layer1;
+    indicator_config layer2;
 } berserker_config;
 
 berserker_config config; 
@@ -109,20 +112,36 @@ enum via_config_enums {
     id_config_num_lock_enabled = 4,
     id_config_num_lock_brightness = 5,
     id_config_num_lock_color = 6,
+    id_config_layer1_enabled = 7,
+    id_config_layer1_brightness = 8,
+    id_config_layer1_color = 9,
+    id_config_layer2_enabled = 10,
+    id_config_layer2_brightness = 11,
+    id_config_layer2_color = 12
 };
 
 
 void eeconfig_init_user(void) {
     // Default values
     config.caps_lock.h       = 0;
-    config.caps_lock.s       = 100;
+    config.caps_lock.s       = 255;
     config.caps_lock.v       = 255;
     config.caps_lock.enabled = true;
 
     config.num_lock.h       = 0;
-    config.num_lock.s       = 100;
+    config.num_lock.s       = 255;
     config.num_lock.v       = 255;
     config.num_lock.enabled = true;
+
+    config.layer1.h       = 85;
+    config.layer1.s       = 255;
+    config.layer1.v       = 255;
+    config.layer1.enabled = true;
+
+    config.layer2.h       = 170;
+    config.layer2.s       = 255;
+    config.layer2.v       = 255;
+    config.layer2.enabled = true;
     eeconfig_update_kb_datablock(&config);
 }
 
@@ -166,6 +185,34 @@ void config_set_value(uint8_t *data) {
             break;
         }
 
+
+        case id_config_layer1_enabled: {
+            config.layer1.enabled = value_data[0];
+            break;
+        }
+        case id_config_layer1_brightness: {
+            config.layer1.v = value_data[0];
+            break;
+        }
+        case id_config_layer1_color: {
+            config.layer1.h = value_data[0];
+            config.layer1.s = value_data[1];
+            break;
+        }
+
+        case id_config_layer2_enabled: {
+            config.layer2.enabled = value_data[0];
+            break;
+        }
+        case id_config_layer2_brightness: {
+            config.layer2.v = value_data[0];
+            break;
+        }
+        case id_config_layer2_color: {
+            config.layer2.h = value_data[0];
+            config.layer2.s = value_data[1];
+            break;
+        }
     }
 }
 
@@ -199,6 +246,34 @@ void config_get_value(uint8_t *data) {
         case id_config_num_lock_color: {
             value_data[0] = config.num_lock.h;
             value_data[1] = config.num_lock.s;
+            break;
+        }
+
+        case id_config_layer1_enabled: {
+            value_data[0] = config.layer1.enabled;
+            break;
+        }
+        case id_config_layer1_brightness: {
+            value_data[0] = config.layer1.v;
+            break;
+        }
+        case id_config_layer1_color: {
+            value_data[0] = config.layer1.h;
+            value_data[1] = config.layer1.s;
+            break;
+        }
+
+        case id_config_layer2_enabled: {
+            value_data[0] = config.layer2.enabled;
+            break;
+        }
+        case id_config_layer2_brightness: {
+            value_data[0] = config.layer2.v;
+            break;
+        }
+        case id_config_layer2_color: {
+            value_data[0] = config.layer2.h;
+            value_data[1] = config.layer2.s;
             break;
         }
     }
@@ -245,6 +320,41 @@ void via_custom_value_command_kb(uint8_t *data,uint8_t length){
 
 __attribute__((weak)) bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     
+
+//    if (host_keyboard_led_state().scroll_lock)
+//        RGB_MATRIX_INDICATOR_SET_COLOR(14,255,0,0);
+
+    bool change = false;
+    RGB rgb = {0, 0, 0};
+
+    if (layer_state_is(_SECOND)) {
+        if(config.layer1.enabled){
+            HSV config_color = {config.layer1.h, config.layer1.s, config.layer1.v};
+            rgb = hsv_to_rgb(config_color);
+            change = true;
+        }
+    } 
+
+    if(layer_state_is(_THIRD)) {
+        if(config.layer2.enabled){
+            HSV config_color = {config.layer2.h, config.layer2.s, config.layer2.v};
+            rgb = hsv_to_rgb(config_color);
+            change = true;
+        }
+        else{
+            //invalide previous change just in case
+            change = false;
+        }
+    }
+
+    if(change){
+        for (uint8_t i = led_min; i < led_max; i++) {
+            
+            if(HAS_FLAGS(g_led_config.flags[i],LED_FLAG_MODIFIER))
+                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+        }
+    }
+
     if (config.caps_lock.enabled && host_keyboard_led_state().caps_lock)
     {
         HSV config_color = {config.caps_lock.h, config.caps_lock.s, config.caps_lock.v};
@@ -260,29 +370,6 @@ __attribute__((weak)) bool rgb_matrix_indicators_advanced_user(uint8_t led_min, 
 
         RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_INDEX,rgb_color.r,rgb_color.g,rgb_color.b);
     }
-
-    if (host_keyboard_led_state().scroll_lock)
-        RGB_MATRIX_INDICATOR_SET_COLOR(14,255,0,0);
-
-    bool change = false;
-    RGB rgb = {0, 0, 0};
-
-    if (layer_state_is(_SECOND)) {
-        rgb.g = 255;
-        change = true;
-    } 
-    if(layer_state_is(_THIRD)) {
-        rgb.g = 0;
-        rgb.b = 255;
-        change = true;
-    }
-
-    if(change)
-        for (uint8_t i = led_min; i < led_max; i++) {
-            
-            if(HAS_FLAGS(g_led_config.flags[i],LED_FLAG_MODIFIER))
-                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-        }
 
     return true;
 }
